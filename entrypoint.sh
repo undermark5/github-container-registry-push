fdc957dcde0ac26c1ac8020c060c4522586b3687
undermark5 <nsshurtz@gmail.com>
2022-04-11 01:01:24 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ GITHUB_OWNER=`echo ${GITHUB_REPOSITORY} | cut -d/ -f1`
 IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
+echo ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 
 # Push image
-docker push ${IMAGE_ID}
+docker push ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
